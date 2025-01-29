82d2112fec917f199cf8fccf1d3e9a87aee72ac8
Nataliia <nkopichinckaya@gmail.com>
2025-01-29 18:08:02 +0100

    add refresh redux

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const contactsSlice = createSlice({
   name: "contacts",
@@ -9,18 +9,23 @@ const contactsSlice = createSlice({
       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
       { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
+    filter: "", // Додаємо фільтр у `state`
   },
   reducers: {
-    addContact: (state, action) => {
-      state.items.push({ id: nanoid(), ...action.payload });
+    addContact(state, action) {
+      state.items.push(action.payload);
     },
-    deleteContact: (state, action) => {
+    deleteContact(state, action) {
       state.items = state.items.filter(
         (contact) => contact.id !== action.payload
       );
     },
+    setFilter(state, action) {
+      state.filter = action.payload;
+    },
   },
 });
 
-export const { addContact, deleteContact } = contactsSlice.actions;
+// ❗ Додаємо експорт `setFilter`
+export const { addContact, deleteContact, setFilter } = contactsSlice.actions;
 export default contactsSlice.reducer;
